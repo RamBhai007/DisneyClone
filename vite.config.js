4f64a922c9f998fd2328815aea2c6a07c5e75baf
RamBhai007 <rammohanrockz007@gmail.com>
2023-09-29 12:16:54 +0530

    depoly setting

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"https://RamBhai007.github.io/Disney_Clone_with_tailwind_css",
   plugins: [react()],
 })
